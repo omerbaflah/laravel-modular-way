60bb9efe0ac8ad4e323f9377d40fc7cfbe4a6623
Omer Baflah <aldonramos@gmail.com>
2022-04-12 16:58:39 +0300

    Update API routes

--- Modules/User/Routes/api.php
+++ Modules/User/Routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Route::name('sessions.')->group(function () {
+//    Route::post('login', [SessionsController::class, 'login'])->name('login');
+//    Route::post('register', [SessionsController::class, 'register'])->name('register');
+//    Route::get('register/activate/{token}', [SessionsController::class, 'activateAccount'])->name('activate-account');
+//    Route::post('register/resend-activation-email', [SessionsController::class, 'resendActivationEmail'])->name('resend-activation-email');
+//
+//    /**
+//     * Here we put protected routes
+//     */
+//    Route::middleware('auth:sanctum')->group(function () {
+//        Route::post('password/change', [SessionsController::class, 'changePassword'])->name('change-password');
+//        Route::post('logout', [SessionsController::class, 'logout'])->name('logout');
+//    });
+//});
