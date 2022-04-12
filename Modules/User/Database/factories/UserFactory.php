2dbb150a83a4eba313995b4d19d45aea549deb02
Omer Baflah <aldonramos@gmail.com>
2022-04-12 16:57:22 +0300

    Update user factory

--- Modules/User/Database/factories/UserFactory.php
+++ Modules/User/Database/factories/UserFactory.php
@@ -1,15 +1,24 @@
 <?php
 
-namespace Database\Factories;
+namespace Modules\User\Database\factories;
 
+use Illuminate\Support\Facades\Hash;
+use Modules\User\Entities\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends Factory
  */
 class UserFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
     /**
      * Define the model's default state.
      *
@@ -21,7 +30,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('123456'), // password
             'remember_token' => Str::random(10),
         ];
     }
