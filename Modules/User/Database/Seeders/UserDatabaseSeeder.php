bfc9de9b85712518f17045f7cbbaa24882470abd
Omer Baflah <aldonramos@gmail.com>
2022-04-14 17:29:19 +0300

    add seeder for user model

--- Modules/User/Database/Seeders/UserDatabaseSeeder.php
+++ Modules/User/Database/Seeders/UserDatabaseSeeder.php
@@ -16,6 +16,9 @@ class UserDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call("OthersTableSeeder");
+        $this->command->getOutput()->title('User Module Seeding');
+        $this->command->getOutput()->title('Seeding Users');
+        $this->call(class: UserTableSeeder::class);
+        $this->command->getOutput()->progressStart( 10);
     }
 }
