b10be3592726eefc98cb4c72a91733e980b7edcf
Omer Baflah <aldonramos@gmail.com>
2022-04-17 16:31:23 +0300

    Update authentications messages

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,8 +13,14 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
+    'failed'           => 'These credentials do not match our records.',
+    'password'         => 'The provided password is incorrect.',
+    'throttle'         => 'Too many login attempts. Please try again in :seconds seconds.',
+    'success_register' => 'you have registered successfully',
+    'logout'           => 'logged out successfully.',
+    'not_authorized'   => 'You are not authorized to do this action.',
+    'success_login'    => 'you have logged in successfully',
+    'verify_email'     => 'You have to verify your email.',
+    'invalid_token'    => 'This activation token is invalid.',
+    'email_verified'   => 'Your email has been verified successfully.',
 ];
