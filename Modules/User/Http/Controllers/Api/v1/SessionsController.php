e23befc3047333a0e58839fe7256aafbf6f3ef0e
Omer Baflah <aldonramos@gmail.com>
2022-04-18 14:06:15 +0300

    Refactoring login process to action pattern

--- Modules/User/Http/Controllers/Api/v1/SessionsController.php
+++ Modules/User/Http/Controllers/Api/v1/SessionsController.php
@@ -5,27 +5,17 @@ namespace Modules\User\Http\Controllers\Api\v1;
 use App\Helpers\ApiResponseHandler;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Auth;
-use Modules\User\DataTransferObjects\LoginDto;
+use Modules\User\Actions\LoginAction;
 use Modules\User\Http\Requests\LoginRequest;
-use Modules\User\Http\Resources\LoginResource;
 
 class SessionsController extends Controller
 {
     public function login(LoginRequest $request)
     {
-        $userData = new LoginDto($request->all());
+        $user = (new LoginAction())($request);
 
-        if (Auth::attempt(['email' => $userData->email,'password' => $userData->password])) {
-
-            $user = Auth::user();
-
-            $tokenResult = $user->createToken('Personal Access Token');
-            $token = $tokenResult->plainTextToken;
-
-            $user['token'] = $token;
-
-            return ApiResponseHandler::respondWithSuccess(__('auth.success_login'), LoginResource::make($user));
+        if ($user && $user['token']) {
+            return ApiResponseHandler::respondWithSuccess(__('auth.success_login'), $user);
         }
 
         return ApiResponseHandler::respondWithError(__('auth.failed'), null, 401);
